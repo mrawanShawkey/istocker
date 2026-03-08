713b8322b032974b30830e81367635f13bce6561
NourAshour <nashour767@gmail.com>
2026-03-08 16:04:01 +0200

    Edit yml file and add categorized ds path to path.py

--- config/paths.py
+++ config/paths.py
@@ -10,16 +10,17 @@ DATA_DIR = BASE_DIR / "data"
 MARKET_DIR = DATA_DIR / "market_data"
 ML_DIR = DATA_DIR / "ml_data"
 RAW_DIR = MARKET_DIR / "raw"
-PROCESSRD_DIR = MARKET_DIR / "processed"
+PROCESSED_DIR = MARKET_DIR / "processed"
 ML_DIR = DATA_DIR / "ml_data"
 
 # -------------------------
 # Market Data Files |
 # -------------------------
 RAW_MARKET_FILE = RAW_DIR / "EGX30_Full_Dataset_Ready.csv"
-CLEANED_MARKET_FILE = PROCESSRD_DIR / "egx30_clean.csv"
+CLEANED_MARKET_FILE = PROCESSED_DIR / "egx30_clean.csv"
 MACRO_FILE = RAW_DIR / "Egypt_Economic_Data.xlsx"
-MACROECNOMIC_ALIGNMENT_FILE = PROCESSRD_DIR / "egx30_with_macro.csv"
+MACROECNOMIC_ALIGNMENT_FILE = PROCESSED_DIR / "egx30_with_macro.csv"
+CATEGORIZED_FILE = PROCESSED_DIR / "egx30_categorized.csv"
 MODELING_DATASET_FILE = ML_DIR / "datasets" / "EGX30_modeling_dataset.csv"
 # -------------------------
 # ML Datasets (Optional for EDA)
