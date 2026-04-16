29fbe40efff46ce63f4e4055c79696d86156d4df
NourAshour <nashour767@gmail.com>
2026-04-16 16:42:51 +0200

    Fixed routes error

--- api/routes.py
+++ api/routes.py
@@ -3,4 +3,36 @@ from flask_login import login_user, logout_user, current_user, login_required
 
 def register_routes(app, db, bcrypt):
     pass
+    ###################AUTH###################
+
+    #Register x2
+
+    #Login
+
+    #Logout
+
+    #Delete Account
+
+    ###################STATIC###################
+
+    #Home
+
+    #Edu
     
+    ###################QUESTIONNAIRE###################
+
+    #Questions X17
+    
+    #Results
+    
+    ###################MARKET###################
+
+    #Market Overview
+
+    #Specific Stock
+
+    ###################USER###################
+
+    #User Profile
+
+    #User Settings
