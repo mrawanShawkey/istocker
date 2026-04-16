ff2184671e71411de4503698a52b67363adeb15b
NourAshour <nashour767@gmail.com>
2026-04-16 16:42:04 +0200

    Added api files, and moved presentation into api and renamed it static, moved html files to templates (Flask expects it)

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,3 @@
-from api.models import User, UserProfile, RiskCategory, RiskAssessment, Question, Option, UserResponse 
-from api.models import Sector, Stock, StockPrice, Prediction, RecommendationSet, Recommendation, MarketIndex, IndexPrice
 from flask import render_template, request, url_for, redirect
 from flask_login import login_user, logout_user, current_user, login_required
 
