cdcaeac173ac66861a2cc993f482869dc4ab1825
arwa303 <arwaaljuhany@gmail.com>
2026-06-19 17:38:42 +0300

    Increase default JWT secret key length for security

--- api/config.py
+++ api/config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///./database.db')
-    JWT_SECRET_KEY = os.environ.get('SECRET_KEY', 'SOME KEY')
+    JWT_SECRET_KEY = os.environ.get('SECRET_KEY', 'istocker-secret-key-min-32-bytes-long-2026')
     SMTP_SERVER = os.environ.get('SMTP_SERVER')
     SMTP_PORT = os.environ.get('SMTP_PORT')
     EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
